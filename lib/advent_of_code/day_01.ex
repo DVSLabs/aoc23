9a9442376231a4366cce617a16b9754a550364df
DVSLabs <52368450+DVSLabs@users.noreply.github.com>
2023-12-01 17:56:12 +1100

    d01.p1 solution

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,7 +1,28 @@
 defmodule AdventOfCode.Day01 do
-  def part1(_args) do
+  def part1(args) do
+    args
+    |> String.split(~r/\n/, trim: true)
+    |> Enum.map(&line_calibration_value/1)
+    |> Enum.sum()
   end
 
   def part2(_args) do
+  end
+
+  # TODO: doesn't work if the string has no numbers
+  defp line_calibration_value(string) do
+    # remove any non-numeric characters
+    number_from_string = String.replace(string, ~r/[^\d]/, "")
+
+    calibration_number = [
+      # first number
+      String.at(number_from_string, 0),
+      # last number, or first number is string has length of 1
+      String.at(number_from_string, -1)
+    ]
+
+    calibration_number
+    |> List.to_string()
+    |> String.to_integer()
   end
 end
